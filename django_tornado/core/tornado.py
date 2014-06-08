5fce467ab2ff006741b6811be755075e1a2f1819
Jeff Buttars <jeffbuttars@gmail.com>
2014-06-07 19:04:34 -0600

    initial framing, based on wsgi handler, for tornado handler. Does not work

--- django_tornado/core/tornado.py
+++ django_tornado/core/tornado.py
@@ -1,2 +1,11 @@
 import django
 from django_tornado.core.handlers.tornado import TornadoHandler
+
+
+def get_tornado_application():
+    """
+    """
+
+    django.setup()
+    return TornadoHandler()
+# get_tornado_application()
