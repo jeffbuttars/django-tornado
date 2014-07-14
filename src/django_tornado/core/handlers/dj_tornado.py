2633038edc5dbc5ee0e54a0ae2db806c822038e5
Jeff Buttars <jeffbuttars@gmail.com>
2014-07-14 16:21:17 -0600

    First working version of our own get_response() method useing a sync response.

--- src/django_tornado/core/handlers/dj_tornado.py
+++ src/django_tornado/core/handlers/dj_tornado.py
@@ -17,10 +17,12 @@ from io import BytesIO
 from django import http
 from django.core import signals
 from django.conf import settings
+from django.core import urlresolvers
 from django.core.handlers import base
 from django.utils import datastructures
 from django.utils.functional import cached_property
 from django.core.urlresolvers import set_script_prefix
+from django.core.exceptions import MiddlewareNotUsed, PermissionDenied, SuspiciousOperation
 try:
     from django.utils.deprecation import RemovedInDjango19Warning
 except ImportError:
@@ -236,6 +238,10 @@ class TornadoHandler(base.BaseHandler):
         logger.debug("TornadoHandler __call__ %s", t_req)
         self.tornado_request = t_req
 
+        self.urlconf = settings.ROOT_URLCONF
+        self.resolver = urlresolvers.RegexURLResolver(r'^/', self.urlconf)
+        self.callback, self.callback_args, self.callback_kwargs = (None, None, None)
+
         # Set up middleware if needed. We couldn't do this earlier, because
         # settings weren't available.
         if self._request_middleware is None:
@@ -272,6 +278,210 @@ class TornadoHandler(base.BaseHandler):
 
         return response
     # __call__()
+
+    def _apply_request_middleware(self, request):
+        """todo: Docstring for _apply_request_middleware
+        
+        :param request: arg description
+        :type request: type description
+        :return:
+        :rtype:
+        """
+        logger.debug("_apply_request_middleware request: %s", request)
+        urlresolvers.set_urlconf(self.urlconf)
+
+        response = None
+        # Apply request middleware
+        for middleware_method in self._request_middleware:
+            response = middleware_method(request)
+            if response:
+                break
+
+        if response is None:
+            if hasattr(request, 'urlconf'):
+                # Reset url resolver with a custom urlconf.
+                self.urlconf = request.urlconf
+                urlresolvers.set_urlconf(self.urlconf)
+                self.resolver = urlresolvers.RegexURLResolver(r'^/', self.urlconf)
+
+            resolver_match = self.resolver.resolve(request.path_info)
+            self.callback, self.callback_args, self.callback_kwargs = resolver_match
+            request.resolver_match = resolver_match
+
+        return response
+    # _apply_request_middleware()
+
+    def _apply_view_midlleware(self, request, response):
+        """todo: Docstring for _apply_view_midlleware
+        
+        :param request: arg description
+        :type request: type description
+        :return:
+        :rtype:
+        """
+    
+        logger.debug("_apply_view_midlleware request: \n%s\n%s", request, response)
+        if response is None:
+            # Apply view middleware
+            for middleware_method in self._view_middleware:
+                response = middleware_method(
+                    request,
+                    self.callback,
+                    self.callback_args,
+                    self.callback_kwargs)
+                if response:
+                    break
+
+        return response
+    # _apply_view_midlleware()
+
+    def _call_view(self, request, response):
+        """todo: Docstring for _call_view
+        
+        :param request: arg description
+        :type request: type description
+        :return:
+        :rtype:
+        """
+        logger.debug("_call_view request: %s", request)
+
+        if response is None:
+            wrapped_callback = self.make_view_atomic(self.callback)
+            try:
+                response = wrapped_callback(request, *self.callback_args, **self.callback_kwargs)
+            except Exception as e:
+                # If the view raised an exception, run it through exception
+                # middleware, and if the exception middleware returns a
+                # response, use that. Otherwise, reraise the exception.
+                for middleware_method in self._exception_middleware:
+                    response = middleware_method(request, e)
+                    if response:
+                        break
+                if response is None:
+                    raise
+        # Complain if the view returned None (a common error).
+        if response is None:
+            if isinstance(self.callback, types.FunctionType):    # FBV
+                view_name = self.callback.__name__
+            else:                                           # CBV
+                view_name = self.callback.__class__.__name__ + '.__call__'
+            raise ValueError("The view %s.%s didn't return an HttpResponse object." % (
+                self.callback.__module__, view_name))
+
+        return response
+    # _call_view()
+
+    def _apply_response_middleware(self, request, response):
+        logger.debug("_apply_response_middleware response %s", request, response)
+        # Apply response middleware, regardless of the response
+        for middleware_method in self._response_middleware:
+            logger.debug("_apply_response_middleware %s", middleware_method)
+            response = middleware_method(request, response)
+        response = self.apply_response_fixes(request, response)
+
+        return response
+    # _apply_response_middleware()
+
+    def _render_template(self, request, response):
+        """todo: Docstring for _render_template
+        
+        :param request: arg description
+        :type request: type description
+        :param response: arg description
+        :type response: type description
+        :return:
+        :rtype:
+        """
+        logger.debug("_render_template \n%s\n%s", request, response)
+        # If the response supports deferred rendering, apply template
+        # response middleware and then render the response
+        if hasattr(response, 'render') and callable(response.render):
+            for middleware_method in self._template_response_middleware:
+                response = middleware_method(request, response)
+            return response.render()
+
+        return response
+    # _render_template()
+
+    def get_response(self, request):
+        "Returns an HttpResponse object for the given HttpRequest"
+        logger.debug("TornadoHandler get_response %s", request) 
+        # return super(TornadoHandler, self).get_response(request)
+
+        resolver = self.resolver
+        response = None
+
+        try:
+            response = self._apply_request_middleware(request)
+            response = self._apply_view_midlleware(request, response)
+            response = self._call_view(request, response)
+            response = self._render_template(request, response)
+        except http.Http404 as e:
+            logger.warning('Not Found: %s', request.path,
+                        extra={
+                            'status_code': 404,
+                            'request': request
+                        })
+            if settings.DEBUG:
+                response = debug.technical_404_response(request, e)
+            else:
+                try:
+                    self.callback, param_dict = resolver.resolve404()
+                    response = self.callback(request, **param_dict)
+                except:
+                    signals.got_request_exception.send(sender=self.__class__, request=request)
+                    response = self.handle_uncaught_exception(request, resolver, sys.exc_info())
+
+        except PermissionDenied:
+            logger.warning(
+                'Forbidden (Permission denied): %s', request.path,
+                extra={
+                    'status_code': 403,
+                    'request': request
+                })
+            try:
+                self.callback, param_dict = resolver.resolve403()
+                response = self.callback(request, **param_dict)
+            except:
+                signals.got_request_exception.send(
+                        sender=self.__class__, request=request)
+                response = self.handle_uncaught_exception(request,
+                        resolver, sys.exc_info())
+
+        except SuspiciousOperation as e:
+            # The request logger receives events for any problematic request
+            # The security logger receives events for all SuspiciousOperations
+            security_logger = logging.getLogger('django.security.%s' %
+                            e.__class__.__name__)
+            security_logger.error(force_text(e))
+
+            try:
+                self.callback, param_dict = resolver.resolve400()
+                response = self.callback(request, **param_dict)
+            except:
+                signals.got_request_exception.send(
+                        sender=self.__class__, request=request)
+                response = self.handle_uncaught_exception(request,
+                        resolver, sys.exc_info())
+
+        except SystemExit:
+            # Allow sys.exit() to actually exit. See tickets #1023 and #4701
+            raise
+
+        except: # Handle everything else.
+            # Get the exception info now, in case another exception is thrown later.
+            signals.got_request_exception.send(sender=self.__class__, request=request)
+            response = self.handle_uncaught_exception(request, resolver, sys.exc_info())
+
+        try:
+            self._apply_response_middleware(request, response)
+        except: # Any exception should be gathered and handled
+            signals.got_request_exception.send(sender=self.__class__, request=request)
+            response = self.handle_uncaught_exception(request, resolver, sys.exc_info())
+
+        return response
+    #get_response()
+
 # TornadoHandler
 
 
