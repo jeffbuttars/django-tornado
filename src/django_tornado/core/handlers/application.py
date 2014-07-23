f9c22ae3976a6dd799a8e35b2b8aaab88ca0d342
Jeff Buttars <jeffbuttars@gmail.com>
2014-07-22 18:28:11 -0600

    Move all async stuff under handlers directory for simplicity.

--- src/django_tornado/core/handlers/application.py
+++ src/django_tornado/core/handlers/application.py
@@ -8,12 +8,12 @@ if tornado_version_major == 3:
     if tornado_version_minor != 2:
         raise TornadoVersionNotSupported(
             "Tornado version %s is not supported by DjangoTornado." % tornado.version)
-    from django_tornado.core.tv32.application import DjangoTornadoRequestHandler, DjangoApplication
+    from django_tornado.core.handlers.tv32.application import DjangoTornadoRequestHandler, DjangoApplication
 elif tornado_version_major == 4:
     if tornado_version_minor != 0:
         raise TornadoVersionNotSupported(
             "Tornado version %s is not supported by DjangoTornado." % tornado.version)
-    from django_tornado.core.tv40.application import DjangoTornadoRequestHandler, DjangoApplication
+    from django_tornado.core.handlers.tv40.application import DjangoTornadoRequestHandler, DjangoApplication
 else:
     raise TornadoVersionNotSupported(
         "Tornado version %s is not supported by DjangoTornado." % tornado.version)
